931cbbc914b4e0a74f70b73acaaf451df06a35da
bakedPotatoLord <918780@dpsk12.net>
2022-10-17 13:33:41 -0600

    added displaymessage function

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ console.log('documentReady')
 let name  = <HTMLInputElement>document.getElementById('name')
 let recip =<HTMLInputElement> document.getElementById('recip')
 let tInput = <HTMLInputElement>document.getElementById('tInput')
-let textarea =  document.getElementById('tArea')
+let textarea =  <HTMLTextAreaElement>document.getElementById('tArea')
 let uuid = document.getElementById('uuid')
 let logout = document.getElementById('logout')
 
@@ -16,17 +16,26 @@ let logout = document.getElementById('logout')
 uuid.innerHTML ="Hello " +Cookies.get('username')
 
 
+function displayMessages(messageArr: Message[]){
+    console.log(messageArr)
+    let displayText:string = ''
+    for(let i of messageArr){
+        displayText+= `${ new Date(i.timestamp).getHours()}:${ new Date(i.timestamp).getMinutes()} ${i.sender} -> ${i.recip} : ${i.text}\n`
+    }
+    textarea.value = displayText
+}
+
 
 //@ts-ignore
 var sock = new SockJS('/echo',"",{sessionId:8});
 sock.onopen = function() {
     console.log('open');
-    
-    sock.send(JSON.stringify(new Message("me","you","yo")));
 };
 
+//setTimeout(()=>{sock.send(JSON.stringify(new Message("me","you","yo"))); },1000)
+
 sock.onmessage = function(e) {
-    console.log('message', e.data);
+    displayMessages(JSON.parse(e.data))
     //sock.close();
 };
 
